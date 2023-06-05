a0c82bdd8d5f8ce0ab907a49aee6b747c2567976
Lucas García <lucasgarcia.developer@gmail.com>
2023-06-05 20:50:24 -0300

    clase 04 - inicio

---
+++ src/clase04-apuntes.js
@@ -0,0 +1,8 @@
+/**
+ * Set Timeout
+ * Nos permite ejecutar una función o procesa fuera del colstack, de forma asíncrona, al final de todas las ejecuciones, aunque el tiempo establecido sea de "0" cero
+ */
+setTimeout(()=>{
+    console.log('console de settimeout')
+}, 0);
+
